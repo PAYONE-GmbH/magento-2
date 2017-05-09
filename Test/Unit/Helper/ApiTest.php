854ea7227bc19fec89abe62f29b7b3be1f501c1c
Mario Dorn <mario.dorn@fatchip.de>
2017-05-09 21:07:04 +0200

    Update the unit test class for the API helper

--- Test/Unit/Helper/ApiTest.php
+++ Test/Unit/Helper/ApiTest.php
@@ -126,7 +126,7 @@ class ApiTest extends \PHPUnit_Framework_TestCase
             );
         $this->payment->method('needsProductInfo')->willReturn(true);
 
-        $result = $this->api->isInvoiceDataNeeded($this->payment);
+        $result = $this->api->isInvoiceDataNeeded($this->payment, null);
         $this->assertTrue($result);
     }
 
@@ -142,7 +142,7 @@ class ApiTest extends \PHPUnit_Framework_TestCase
             );
         $this->payment->method('needsProductInfo')->willReturn(false);
 
-        $result = $this->api->isInvoiceDataNeeded($this->payment);
+        $result = $this->api->isInvoiceDataNeeded($this->payment, null);
         $this->assertFalse($result);
     }
 
