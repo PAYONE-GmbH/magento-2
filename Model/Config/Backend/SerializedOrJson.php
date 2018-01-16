ab5477de9a01b44b9a020354135b7e4149593840
Robert Müller <robert.mueller@fatchip.de>
2018-01-16 16:35:21 +0100

    MAG2-42 - Fixed problem with backward compatibility

---
+++ Model/Config/Backend/SerializedOrJson.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * PAYONE Magento 2 Connector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PAYONE Magento 2 Connector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PAYONE Magento 2 Connector. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ *
+ * @category  Payone
+ * @package   Payone_Magento2_Plugin
+ * @author    FATCHIP GmbH <support@fatchip.de>
+ * @copyright 2003 - 2016 Payone GmbH
+ * @license   <http://www.gnu.org/licenses/> GNU Lesser General Public License
+ * @link      http://www.payone.de
+ */
+
+namespace Payone\Core\Model\Config\Backend;
+
+use Magento\Framework\App\Config\Value;
+
+class SerializedOrJson extends Value
+{
+    /**
+     * PAYONE Base helper
+     *
+     * @var \Payone\Core\Helper\Base
+     */
+    protected $baseHelper;
+
+    /**
+     * @param \Magento\Framework\Model\Context                             $context
+     * @param \Magento\Framework\Registry                                  $registry
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface           $config
+     * @param \Magento\Framework\App\Cache\TypeListInterface               $cacheTypeList
+     * @param \Payone\Core\Helper\Base                                     $baseHelper
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null           $resourceCollection
+     * @param array                                                        $data
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Payone\Core\Helper\Base $baseHelper,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+        $this->baseHelper = $baseHelper;
+    }
+
+    /**
+     * @return void
+     */
+    protected function _afterLoad()
+    {
+        if (!is_array($this->getValue())) {
+            $value = $this->getValue();
+            $this->setValue(empty($value) ? false : $this->baseHelper->unserialize($value));
+        }
+    }
+
+    /**
+     * @return $this
+     */
+    public function beforeSave()
+    {
+        $value = $this->getValue();
+        if (is_array($value)) {
+            unset($value['__empty']);
+            $value = $this->baseHelper->serialize($value);
+        }
+        $this->setValue($value);
+        return parent::beforeSave();
+    }
+}
---
+++ Test/Unit/Model/Config/Backend/SerializedOrJsonTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * PAYONE Magento 2 Connector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PAYONE Magento 2 Connector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PAYONE Magento 2 Connector. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ *
+ * @category  Payone
+ * @package   Payone_Magento2_Plugin
+ * @author    FATCHIP GmbH <support@fatchip.de>
+ * @copyright 2003 - 2017 Payone GmbH
+ * @license   <http://www.gnu.org/licenses/> GNU Lesser General Public License
+ * @link      http://www.payone.de
+ */
+
+namespace Payone\Core\Test\Unit\Model\Config\Backend;
+
+use Payone\Core\Model\Config\Backend\SerializedOrJson as ClassToTest;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Payone\Core\Test\Unit\BaseTestCase;
+use Payone\Core\Test\Unit\PayoneObjectManager;
+
+
+class SerializedOrJsonTest extends BaseTestCase
+{
+    /**
+     * @var ClassToTest
+     */
+    private $classToTest;
+
+    /**
+     * @var ObjectManager|PayoneObjectManager
+     */
+    private $objectManager;
+
+    protected function setUp()
+    {
+        $this->objectManager = $this->getObjectManager();
+
+        $this->classToTest = $this->objectManager->getObject(ClassToTest::class, [
+
+        ]);
+    }
+
+    public function testBeforeSave()
+    {
+        $this->classToTest->setValue(['test' => '123']);
+        $result = $this->classToTest->beforeSave();
+        $this->assertInstanceOf(ClassToTest::class, $result);
+    }
+
+    public function testAfterLoad()
+    {
+        $this->classToTest->setValue(json_encode(['test' => '123']));
+        $result = $this->classToTest->afterLoad();
+        $this->assertInstanceOf(ClassToTest::class, $result);
+    }
+}
