c797b98d84d1d3cd900ffbf21e94ab36a651584b
Robert Müller <robert.mueller@fatchip.de>
2020-03-18 15:11:04 +0100

    MAG2-117 - Removed BillSafe and Klarna Invoice

--- Test/Unit/Model/Api/Request/UpdateuserTest.php
+++ Test/Unit/Model/Api/Request/UpdateuserTest.php
@@ -88,7 +88,7 @@ class UpdateuserTest extends BaseTestCase
 
         $payment = $this->getMockBuilder(PayoneMethod::class)->disableOriginalConstructor()->getMock();
         $payment->method('getOperationMode')->willReturn('test');
-        $payment->method('getCode')->willReturn(PayoneConfig::METHOD_KLARNA);
+        $payment->method('getCode')->willReturn(PayoneConfig::METHOD_CASH_ON_DELIVERY);
 
         $response = ['status' => 'APPROVED'];
         $this->apiHelper->method('sendApiRequest')->willReturn($response);
--- Test/Unit/Observer/CheckoutSubmitBeforeTest.php
+++ Test/Unit/Observer/CheckoutSubmitBeforeTest.php
@@ -171,7 +171,7 @@ class CheckoutSubmitBeforeTest extends BaseTestCase
         $this->consumerscoreHelper->expects($this->any())
             ->method('getAllowedMethodsForScore')
             ->willReturnMap([
-                ['Y', [PayoneConfig::METHOD_ADVANCE_PAYMENT, PayoneConfig::METHOD_BILLSAFE]],
+                ['Y', [PayoneConfig::METHOD_ADVANCE_PAYMENT, PayoneConfig::METHOD_CASH_ON_DELIVERY]],
                 ['R', [PayoneConfig::METHOD_DEBIT, PayoneConfig::METHOD_CREDITCARD]]
             ]);
 
@@ -342,7 +342,7 @@ class CheckoutSubmitBeforeTest extends BaseTestCase
         $this->consumerscoreHelper->expects($this->any())
             ->method('getAllowedMethodsForScore')
             ->willReturnMap([
-                ['Y', [PayoneConfig::METHOD_ADVANCE_PAYMENT, PayoneConfig::METHOD_BILLSAFE]],
+                ['Y', [PayoneConfig::METHOD_ADVANCE_PAYMENT, PayoneConfig::METHOD_CASH_ON_DELIVERY]],
                 ['R', [PayoneConfig::METHOD_DEBIT, PayoneConfig::METHOD_CREDITCARD]]
             ]);
 
