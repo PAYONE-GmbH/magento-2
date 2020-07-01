ad7d7871c08d3decca168a29797d77f2b36a8b44
Timo Kuchel <timo.kuchel@bspayone.com>
2020-07-01 11:10:43 +0200

    version bump to 3.0.0

--- Model/PayoneConfig.php
+++ Model/PayoneConfig.php
@@ -32,7 +32,7 @@ namespace Payone\Core\Model;
 abstract class PayoneConfig
 {
     /* Module version */
-    const MODULE_VERSION = '2.8.1';
+    const MODULE_VERSION = '3.0.0';
 
     /* Authorization request types */
     const REQUEST_TYPE_PREAUTHORIZATION = 'preauthorization';
