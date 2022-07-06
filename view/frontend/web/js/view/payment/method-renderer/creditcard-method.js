1c64c7e07cc55bf37c999c8c7923a8afefdd2cef
Dominik Krebs <dominik.krebs@netzkollektiv.com>
2022-07-06 11:10:21 +0200

    allow academic title in credit card holder name

--- view/frontend/web/js/view/payment/method-renderer/creditcard-method.js
+++ view/frontend/web/js/view/payment/method-renderer/creditcard-method.js
@@ -178,7 +178,7 @@ define(
                 return false;
             },
             isCardholderDataValid: function (sCardholder) {
-                if (sCardholder.search(/[^a-zA-ZÄäÖöÜüß\- ]+/) === -1) {
+                if (sCardholder.search(/[^a-zA-ZÄäÖöÜüß\-\. ]+/) === -1) {
                     return true;
                 }
                 return false;
