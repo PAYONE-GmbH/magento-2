13f83a1d61dcdf50a281fcfeaae3cfadc95b6bf1
Jan Teuber <jan.teuber@payone.com>
2023-01-19 14:56:18 +0100

    version bump to 370

--- Model/PayoneConfig.php
+++ Model/PayoneConfig.php
@@ -32,7 +32,7 @@ namespace Payone\Core\Model;
 abstract class PayoneConfig
 {
     /* Module version */
-    const MODULE_VERSION = '3.6.0';
+    const MODULE_VERSION = '3.7.0';
 
     /* Authorization request types */
     const REQUEST_TYPE_PREAUTHORIZATION = 'preauthorization';
