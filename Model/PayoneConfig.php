8398c61920771961b72cf8191bf4a60c912c9fab
Teuber <jan.teuber@worldline.com>
2025-07-01 14:32:49 +0200

    version bump to 3150

--- Model/PayoneConfig.php
+++ Model/PayoneConfig.php
@@ -32,7 +32,7 @@ namespace Payone\Core\Model;
 abstract class PayoneConfig
 {
     /* Module version */
-    const MODULE_VERSION = '3.14.2';
+    const MODULE_VERSION = '3.15.0';
 
     /* Authorization request types */
     const REQUEST_TYPE_PREAUTHORIZATION = 'preauthorization';
