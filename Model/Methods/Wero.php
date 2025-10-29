7202542bfb6491d7e9d29c2a569c23ac6acc3a25
Robert Müller <robert.mueller@fatchip.de>
2025-10-29 12:25:29 +0100

    MAG2-363 - Added Wero payment method (not finished)

---
+++ Model/Methods/Wero.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * PAYONE Magento 2 Connector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PAYONE Magento 2 Connector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PAYONE Magento 2 Connector. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 8
+ *
+ * @category  Payone
+ * @package   Payone_Magento2_Plugin
+ * @author    FATCHIP GmbH <support@fatchip.de>
+ * @copyright 2003 - 2025 Payone GmbH
+ * @license   <http://www.gnu.org/licenses/> GNU Lesser General Public License
+ * @link      http://www.payone.de
+ */
+
+namespace Payone\Core\Model\Methods;
+
+use Magento\Sales\Model\Order;
+use Payone\Core\Model\PayoneConfig;
+
+/**
+ * Model for Wero payment method
+ */
+class Wero extends PayoneMethod
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $_code = PayoneConfig::METHOD_WERO;
+
+    /**
+     * Info instructions block path
+     *
+     * @var string
+     */
+    protected $_infoBlockType = 'Payone\Core\Block\Info\ClearingReference';
+
+    /**
+     * Clearingtype for PAYONE authorization request
+     *
+     * @var string
+     */
+    protected $sClearingtype = 'wlt';
+
+    /**
+     * Wallettype for PAYONE requests
+     *
+     * @var string|bool
+     */
+    protected $sWallettype = 'WRO';
+
+    /**
+     * Determines if the redirect-parameters have to be added
+     * to the authorization-request
+     *
+     * @var bool
+     */
+    protected $blNeedsRedirectUrls = true;
+
+    /**
+     * Return parameters specific to this payment type
+     *
+     * @param  Order $oOrder
+     * @return array
+     */
+    public function getPaymentSpecificParameters(Order $oOrder)
+    {
+        return [
+            'wallettype' => $this->getWallettype(),
+        ];
+    }
+}
